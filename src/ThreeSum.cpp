5e40ff50c3e43e9914026fb62beecce2efe92195
nyankosama <825138000@qq.com>
2014-11-12 23:17:23 -0800

    update cpp-version

---
+++ src/ThreeSum.cpp
@@ -0,0 +1,71 @@
+/*=============================================================================
+#
+# Author: Nyankosama email:i@nyankosama.com
+#
+# Last modified: 2014-11-12 22:56
+#
+# Filename: ThreeSum.cpp
+#
+# Description:
+#   @Problem: ttps://oj.leetcode.com/problems/3sum/
+#   @tag: array two-pointers
+#
+=============================================================================*/
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int> > threeSum(int num[], int n) {
+        sort(num, num + n);
+        set<vector<int> > set;
+        for (int i = 0; i < n - 2; i++) {
+            int start = i + 1, end = n - 1;
+            while(start < end) {
+                int a = num[i], b = num[start], c = num[end];
+                int sum = a + b + c;
+                if (sum > 0) {
+                    end = end - 1;
+                } else if(sum < 0) {
+                    start = start + 1;
+                } else {
+                    vector<int> oneRet;
+                    oneRet.push_back(a);
+                    oneRet.push_back(b);
+                    oneRet.push_back(c);
+                    set.insert(oneRet);
+                    end = end - 1;
+                    start = start + 1;
+                }
+            }
+        }
+        vector<vector<int> > result;
+        for (auto vec: set) {
+            vector<int> oneVec;
+            for (auto item:vec) {
+                oneVec.push_back(item);
+            }
+            result.push_back(oneVec);
+        }
+        return result;
+    }
+};
+
+int main() {
+    int array2[] = {-1, 0, 1, 2, -1, -4};
+    Solution s;
+    auto resultVec = s.threeSum(array2, 6);
+    for (auto vec:resultVec) {
+        for (auto item:vec) {
+            cout << item << " ";
+        }
+        cout << endl;
+    }
+}
+
+
---
+++ src/ThreeSumClosest.cpp
@@ -0,0 +1,62 @@
+/*=============================================================================
+#
+# Author: Nyankosama email:i@nyankosama.com
+#
+# Last modified: 2014-11-12 22:56
+#
+# Filename: ThreeSum.cpp
+#
+# Description:
+#   @Problem: https://oj.leetcode.com/problems/3sum-closest/
+#   @tag: array two-pointers
+#
+=============================================================================*/
+
+#include <algorithm>
+#include <iostream>
+#include <limits>
+#include <cstdlib>
+
+using namespace std;
+
+class Solution {
+public:
+    int threeSumClosest(int num[], int n, int target) {
+        sort(num, num + n);
+        const int MAX_VALUE = numeric_limits<int>::max();
+        int totalResult = target + MAX_VALUE;
+        for (int i = 0; i < n - 2; i++) {
+            int start = i + 1;
+            int end = n - 1;
+            int result = target + MAX_VALUE;
+            while (start < end) {
+                int sum = num[i] + num[start] + num[end];
+                int diff = sum - target;
+                if (diff > 0) {
+                    end = end - 1;
+                } else if (diff < 0) {
+                    start = start + 1;
+                } else {
+                    return target;
+                }
+                if (abs(diff) < abs(result - target)) result = sum;
+            }
+            if (abs(result - target) < abs(totalResult - target)) {
+                totalResult = result;
+            }
+        }
+        return totalResult;
+    }
+};
+
+
+int main() {
+    int array1[] = {-1, 2, 1, -4};
+    Solution s;
+    bool ret = s.threeSumClosest(array1, 4, 1) == 2;
+    cout << ret << endl;
+}
+
+
+
+

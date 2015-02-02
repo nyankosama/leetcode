fab230ba138ad132720588c94a453b5e96d5e2e8
nyankosama <825138000@qq.com>
2015-02-01 18:43:42 -0800

    update MaxSubArrayProduct

--- src/MaximumProductSubarray2.cpp
+++ src/MaximumProductSubarray2.cpp
@@ -18,6 +18,16 @@
 
 using namespace std;
 
+int max3(int a, int b, int c) {
+    if (a >= b && a >= c) return a;
+    else return max3(b, c, a);
+}
+
+int min3(int a, int b, int c){
+    if (a <= b && a <= c) return a;
+    else return min3(b, c, a);
+}
+
 class Solution {
 public:
     int maxProduct(int A[], int n) {
@@ -25,22 +35,23 @@ public:
         int max_tmp = A[0];
         int min_tmp = A[0];
         int max_product = A[0];
-        for (int i = 1; i < n; i++){
+        for (int i = 1; i < n; i++) {
             int a = A[i] * max_tmp;
             int b = A[i] * min_tmp;
-            max_tmp = max(max(a, b), A[i]);
-            min_tmp = min(min(a, b), A[i]);
+            max_tmp = max3(a, b, A[i]);
+            min_tmp = min3(a, b, A[i]);
             max_product = max(max_product, max_tmp);
         }
         return max_product;
-    } 
+    }
 };
 
 
-int main(){
+int main() {
     int A[5] = {1, 2, 3, -1, 100};
     int B[2] = {-3, -4};
     Solution s;
     cout << s.maxProduct(A, 5) << endl;
     cout << s.maxProduct(B, 2) << endl;
 }
+
